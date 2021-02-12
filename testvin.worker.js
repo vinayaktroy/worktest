a98f92d5046f262aec5f913a2d9ed26663383e2a
unknown <vinayak.troy@gmail.com>
2021-02-13 00:54:13 +0530

    changes

--- testvin.worker.js
+++ testvin.worker.js
@@ -1,1 +1,15 @@
 
+ addEventListener('message', ({ data }) => {
+    const response = 'worker response to'+data;
+    console.log(response);
+    fxn(data)
+  });
+
+const fxn = function(data){
+    console.log('in fxn')
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => {
+        postMessage(json);
+    });
+  }
